37cedc119103ddea7457c35be649f1a3dfca04c7
gusti126 <gustidoang936@gmail.com>
2021-06-22 18:34:37 +0700

    menambahkan component kategori

---
+++ components/kategori.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="flex justify-between">
+         <div>
+           <div class="bg-gray-400 rounded-full h-20 w-20 mt-5 mx-auto"></div>
+           <div class="text-center mt-2">
+             Kategori satu
+           </div>
+         </div>
+         <div>
+           <div class="bg-gray-400 rounded-full h-20 w-20 mt-5 mx-auto"></div>
+           <div class="text-center mt-2">
+             Kategori dua
+           </div>
+         </div>
+         <div>
+           <div class="bg-gray-400 rounded-full h-20 w-20 mt-5 mx-auto"></div>
+           <div class="text-center mt-2">
+             Kategori tiga
+           </div>
+         </div>
+         <div>
+           <div class="bg-gray-400 rounded-full h-20 w-20 mt-5 mx-auto"></div>
+           <div class="text-center mt-2">
+             Kategori empat
+           </div>
+         </div>
+         <div>
+           <div class="bg-gray-400 rounded-full h-20 w-20 mt-5 mx-auto"></div>
+           <div class="text-center mt-2">
+             Kategori lima
+           </div>
+         </div>
+         <div>
+           <div class="bg-gray-400 rounded-full h-20 w-20 mt-5 mx-auto"></div>
+           <div class="text-center mt-2">
+             Kategori enam
+           </div>
+         </div>
+         <div>
+           <div class="bg-gray-400 rounded-full h-20 w-20 mt-5 mx-auto"></div>
+           <div class="text-center mt-2">
+             Kategori tujuh
+           </div>
+         </div>
+       </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <navbar />
+  <div>
+    <navbar />
+    <!-- banner header -->
+    <div>
+      <img src="~/assets/banner/header-banner.png" alt="">
+    </div>
+     <!--end banner header -->
+     <!-- kategori -->
+     <div class=" md:px-10 px-2">
+       <div class="text-2xl font-medium mt-4">Kategori</div>
+       <kategori />
+     </div>
+     <!-- endkategori -->
+  </div>
 </template>
 
 <script>
