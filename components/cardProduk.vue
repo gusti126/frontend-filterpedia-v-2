0edb8011b9e353de3aac8b943bf0c65eff26ba64
gusti126 <gustidoang936@gmail.com>
2021-06-22 20:29:53 +0700

    menambahkan components cardProduk

---
+++ components/cardProduk.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bg-white rounded-xl p-3 border shadow-lg">
+    <div>
+      <img
+        :src="require(`~/assets/product/${nameImage}`)"
+        :alt="nameImage"
+        class="rounded-xl"
+      />
+    </div>
+    <div class="font-semibold text-xl text-gray-800 mt-2">{{ title }}</div>
+    <div class="font-normal text-xl text-blue-800">Rp.{{ price }}</div>
+    <div class="flex mt-2">
+        <div class="bg-blue-200 text-blue-500 text-sm my-auto px-1 font-bold mr-2">
+            70%
+        </div>
+      <div class="font-normal text-lg italic line-through text-gray-500">
+        Rp.{{ diskon }}
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5 my-auto ml-4 text-yellow-500"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+        />
+      </svg> (200)
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    price: Number,
+    nameImage: String,
+    diskon: Number,
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,43 @@
     <navbar />
     <!-- banner header -->
     <div>
-      <img src="~/assets/banner/header-banner.png" alt="">
+      <img src="~/assets/banner/header-banner.png" alt="" />
     </div>
-     <!--end banner header -->
-     <!-- kategori -->
-     <div class=" md:px-10 px-2">
-       <div class="text-2xl font-medium mt-4">Kategori</div>
-       <kategori />
-     </div>
-     <!-- endkategori -->
+    <!--end banner header -->
+    <div class="md:px-10 px-2">
+      <div class="text-2xl font-medium mt-4">Kategori</div>
+      <kategori />
+
+      <div class="mt-6">Product Populer</div>
+      <div class="grid md:grid-cols-12 grid-cols-12 grid-flow-row gap-2 md:gap-4">
+        <div class="col-span-3">
+          <nuxt-link to="/productdetail">
+            <card-produk nameImage="Rectangle 20-1.jpg" diskon="80000" price="70000" title="Gusti Maulana Rizkia" />
+          </nuxt-link>
+        </div>
+        <div class="col-span-3">
+          <nuxt-link to="/productdetail">
+            <card-produk nameImage="Rectangle 20.jpg" diskon="80000" price="70000" title="Gusti Maulana Rizkia" />
+          </nuxt-link>
+        </div>
+        <div class="col-span-3">
+          <nuxt-link to="/productdetail">
+            <card-produk nameImage="Rectangle 20-3.jpg" diskon="80000" price="70000" title="Gusti Maulana Rizkia" />
+          </nuxt-link>
+        </div>
+        <div class="col-span-3">
+          <nuxt-link to="/productdetail">
+            <card-produk nameImage="Rectangle 20-2.jpg" diskon="80000" price="70000" title="Gusti Maulana Rizkia" />
+          </nuxt-link>
+        </div>
+        <div class="col-span-3">
+          <nuxt-link to="/productdetail">
+            <card-produk nameImage="Rectangle 20-1.jpg" diskon="80000" price="70000" title="Gusti Maulana Rizkia" />
+          </nuxt-link>
+        </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
@@ -19,3 +47,5 @@
 export default {}
 </script>
 
+
+CardProduk
