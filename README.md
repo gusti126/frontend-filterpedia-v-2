07be568e3880fc3a7e47defaca3850543b534eb7
gusti126 <gustidoang936@gmail.com>
2021-07-05 15:52:39 +0700

    menulis dokumentasi readme

--- README.md
+++ README.md
@@ -20,19 +20,22 @@ $ npm run generate
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
 
 ### Layout
+
 1. default
-    secara default layout menggunakan bawaan nuxtjs
+   secara default layout menggunakan bawaan nuxtjs
 
 2. dashboard
-    layout dashboard untuk page
-    - profile
-    - pesanan saya
-    - riwayat pembelian
-    - pengaturan
-    - logout
+   layout dashboard untuk page
+   - profile
+   - pesanan saya
+   - riwayat pembelian
+   - pengaturan
+   - logout
 
 contoh implementasi layout dashboard
+
 > pages/pesanan.vue
+
 ```
 <template>
     <div>
@@ -45,3 +48,31 @@ export default {
 }
 </script>
 ```
+
+### Pages
+
+1. index
+2. belilangsung
+3. chekoutsuccess
+4. keranjang
+5. login
+6. register
+7. poructdetail
+   > pada page terdapat dua folder dashboard dan mobile. folder mobile dimana tampilan yang memangkhusus mobile view
+8. dashboard
+   - pesanan
+   - profile
+   - setting
+9. mobile
+   - explore
+   - menu-mobile
+
+## Components
+
+1. bannerHeader untuk banner header bisa di manfaatkan untuk promosi atau pemasangan info pamflet
+2. botfooter digunakan untuk menampilkan footer di bawah website
+3. cardPesanan untuk menampilkan produk produk yang di pesan oleh user di gunakan di page _/pesanan_
+4. cardProduk untuk menampilkan produk yang berisi title/name, harga, diskon
+5. mobile-addcart untuk bottom addcart pada tampilan mobile dan tombol sudah di pasang sweetalert
+6. nav-mobile untuk navbar bottom pada tampilan mobile
+7. navbar untuk navigasi bar
--- components/navbar.vue
+++ components/navbar.vue
@@ -50,7 +50,7 @@
               xmlns="http://www.w3.org/2000/svg"
               class="
                 my-auto
-                ml-4
+                mx-4
                 h-8
                 w-8
                 text-white
@@ -86,7 +86,7 @@
               >{{ keranjang.length }}
             </span>
           </nuxt-link>
-          <div class="ml-2 md:ml-6 my-auto">
+          <div class="ml-4 md:ml-6 my-auto">
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -104,6 +104,42 @@
               </svg>
             </div>
           </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="
+              h-9
+              w-9
+              ml-4
+              md:h-8 md:w-8
+              text-white
+              my-auto
+              md:ml-4 md:text-blue-700
+            "
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+          </svg>
+          <span
+            class="
+              absolute
+              top-3
+              right-1
+              text-white
+              bg-green-600
+              rounded-full
+              px-1
+              text-xs
+            "
+          >
+            4
+          </span>
           <nuxt-link
             to="/login"
             class="
