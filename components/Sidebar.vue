49cd299c6dbaf2e5d1ce9f45e4015519650b1ee8
gusti126 <gustidoang936@gmail.com>
2021-07-07 17:01:20 +0700

    edit sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -77,7 +77,7 @@
             d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
           />
         </svg>
-        <div class="my-auto">Riwayat Pembelian</div>
+        <div class="my-auto">Pesanan di proses</div>
       </div>
     </nuxt-link>
     <nuxt-link to="setting">
